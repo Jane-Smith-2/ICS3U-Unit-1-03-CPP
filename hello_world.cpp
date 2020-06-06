13e863e4b0bc87f2644adb3fd3de20b9758491a9
Jane Smith <jame.smith@mths.ca>
2020-06-06 13:16:13 +0000

    change file name

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by Mr. Coxall
+// Created on June 2020
+// This is a "Hello world" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
